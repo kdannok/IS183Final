370e3a016b46c86e7d4eb5999c66ed439f06df69
Kdannok <kdannok@mail.fresnostate.edu>
2017-05-18 17:28:06 -0700

    Exam End

--- src/app/tanks/tank/tank.component.ts
+++ src/app/tanks/tank/tank.component.ts
@@ -7,14 +7,34 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './tank.component.html',
   styleUrls: ['./tank.component.css']
 })
-export class TankComponent implements OnInit {
+export class TankComponent implements OnInit {    
 
   tank: Object;
 
-  constructor() { }
+  constructor(
+    private TankService: TankService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+    ) {}
 
   ngOnInit() {
-  
+  console.log(this.activatedRoute.snapshot.params['id'])
+    this.TankService.getTankById(this.activatedRoute.snapshot.params['id'])
+      .then((resp) => {
+        console.log('resp book', resp);
+        this.tank = resp;
+     });
+  }
+
+  updateTank(id, tank:Object) {
+    console.log('tank', tank);
+    
+    this.TankService.updateTank(id, tank).then((resp) => {
+      console.log('resp', resp);
+      if(resp) {
+        this.router.navigate(['tanks']);
+      }
+    });
   }
 
 }
